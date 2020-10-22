f9a9a551ae336a36e33211dc2449230ca872a454
Hristiyan <hristiyan.ganchov.com>
2020-10-22 21:31:35 +0300

    Add project files.

---
+++ RRB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RRB.Startup))]
+namespace RRB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
