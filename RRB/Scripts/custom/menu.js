c919910c948f0d4cfa3eb478ab77324096c8a9ec
Stefan Kalenderov <stefankalenderov@gmail.com>
2020-11-04 01:04:45 +0200

    added styling for navigation

---
+++ RRB/Scripts/custom/menu.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    var s = $("header nav");
+    var pos = s.position();
+    $(window).scroll(function () {
+        var windowpos = $(window).scrollTop();
+
+        if (windowpos + 50 >= pos.top) {
+            setTimeout(function () {
+                s.removeClass("stick-none");
+                s.addClass("stick");
+            }, 100);
+        }
+
+        if (windowpos === 0) {
+            setTimeout(function () {
+                s.addClass("stick-none");
+                s.removeClass("stick");
+            }, 100);
+        }
+
+        console.log(windowpos === 0)
+    });
+});
