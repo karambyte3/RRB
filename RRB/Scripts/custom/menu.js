6c8c73ed091bd9d8e4bf3084b1a1bff2835f3dc1
Stefan Kalenderov <stefankalenderov@gmail.com>
2020-11-08 22:05:52 +0200

    added design changes

--- RRB/Scripts/custom/menu.js
+++ RRB/Scripts/custom/menu.js
@@ -20,4 +20,20 @@
 
         console.log(windowpos === 0)
     });
+
+    $('#toggleMenu').on('click', function (e) {
+        var $this = $('#toggleMenu');
+        var $menu = $("header");
+        var $navEl = $("header nav");
+
+        if ($this.hasClass('is-active')) {
+            $this.removeClass('is-active');
+            $menu.removeClass('mobile-menu-expanded');
+            $menu.addClass('mobile-menu-collapsed');
+        } else {
+            $this.addClass('is-active');
+            $menu.addClass('mobile-menu-expanded');
+            $menu.removeClass('mobile-menu-collapsed');
+        }
+    })
 });
